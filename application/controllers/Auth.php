4bc5e0cfe5ab5141b684fd730d8218636bb4d731
Daniel2024Guzman <daniel2guzman11@gmail.com>
2024-08-21 17:45:38 +0200

    Creacion de la funcion forgotPassword en la carpeta Auth del controlador. El codigo permite enviar un enlace al correo del cual olvido su contraseña para reestablecer su constraseña

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -37,4 +37,46 @@ class Auth extends CI_Controller {
         $this->session->unset_userdata('user_id');
         redirect(base_url('Auth'));
     }
+
+    function forgotPassword()
+    {
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            if($this->User->verificar_unico_email(-$this->input->post('email')))
+            {
+                $user = $this->User->recuperar_email_user($this->input->post('email'));
+                echo($user->id);
+                $string = time().$user->id.$user->email;
+                $hash_string = hash('sha256', $string);
+                $currentData = date('y-m-d-h:i');
+                $hash_expiry = date('Y-m-d H:i',strtotime($currentDate. ' + 1 days'));
+                $data = array(
+                    'hash_key'=>$hash_string,
+                    'hash_expiry'=>$hash_expiry,);
+                $resetLink = base_url().'Auth/reset_password?hash='.$hash_string;
+                $message = '<p>El enlace para restablecer su contraseña está aquí:</p>'.$resetLink;
+                $subject = "Enlace para restablecer contraseña";
+                $sentStatus = $this->sendEmail($user->email,$subject,$message);
+                if($sentStatus==true)
+                {
+                    $this->User->actualizar_usuario($user->id, $data);
+                    $data['success'] = 'El enlace para reestablecer la contraseña se envió correctamente';
+                    view('auth/forgot_password', $data);
+                }
+                else
+                {
+                    $data['error'] = 'Error al enviar correo electrónico';
+                    view('auth/forgot_password', $data);
+                }
+            }
+            else
+            {
+                $data['error'] = 'El correo no existe';
+                view('auth/forgot_password', $data);
+            }
+        }
+        else
+        {
+            view('auth/forgot_password');
+        }
+    }
 }
